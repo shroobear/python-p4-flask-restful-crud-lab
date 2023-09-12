d4454ef3c14d8f98ff20b2545f5b53f9da894f10
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-09-11 20:31:14 -0400

    lab complete

--- server/app.py
+++ server/app.py
@@ -47,6 +47,43 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+        plant = Plant.query.filter(Plant.id == id).first()
+        data = request.get_json()
+
+        for attr, value in data.items():
+            if attr == "is_in_stock":
+                value = bool(value)
+            setattr(plant, attr, value)
+
+        db.session.add(plant)
+        db.session.commit()
+
+        response_dict = plant.to_dict()
+
+        response = make_response(
+            response_dict,
+            200,
+        )
+
+        return response
+    
+    def delete(self, id):
+        plant = Plant.query.filter(Plant.id == id).first()
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        response_dict = {
+            "": ""
+        }
+
+        response = make_response(
+            response_dict,
+            204,
+        )
+
+        return response
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
